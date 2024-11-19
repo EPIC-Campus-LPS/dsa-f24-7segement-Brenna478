d548ea1db681c01d577056118a1559a0a5b518dc
Brenna478 <woodsbrenna77@gmail.com>
2024-11-19 10:43:02 -0700

    Update stopwatch1.py

--- stopwatch1.py
+++ stopwatch1.py
@@ -22,19 +22,33 @@ tm.write([0,0,0,0])
 tm.brightness(7)
 time.sleep(1)
 
-
+#turn the light off to start 
 GPIO.output(Blue, GPIO.LOW)
+#while the code has power 
 while True: 
+	#if the button is pressed 
 	if GPIO.input(Control) == GPIO.LOW:
+		#wait one second for the timer to start 
 		time.sleep(1)
+		#show how many variables can be displayed
 		tm.write ([0, 0, 0, 0]) 
+		#define count
 		count = 0000 
+		#while this program is running
 		while True:
+			#turn the light on
 			GPIO.output(Blue, GPIO.HIGH)
+			#have the stopwatch tick up
 			count += 1 
+			#only tick up once per second
 			time.sleep(1)
+			#show the count on the display 
 			tm.show(str(count))
+			#when the button is pressed again 
 			if GPIO.input(Control) == GPIO.LOW:
+				#turn off the light
 				GPIO.output(Blue, GPIO.LOW)
+				#take into account the button kick back
 				time.sleep(0.5) 
+				#break back to the first while true
 				break 
